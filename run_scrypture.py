5e0a6e2287ffae629f70ad1ac56f157494caa1bb
Moses Schwartz <mschwar1@bechtel.com>
2015-05-19 12:30:46 -0700

    config file now specified in run script

--- run_scrypture.py
+++ run_scrypture.py
@@ -1,16 +1,11 @@
-from scrypture.scrypture import app
+from scrypture.scrypture import app, load_scripts, load_api, load_config
 import logging
 import sys
 
-# Try loading a local_config.py file
-#if os.path.isfile('local_config.py'):
-#    app.config = AppConfig(app, 'local_config.py')
-# If it isn't there, import config.py
-# This is done to make it harder to lose local config changes when updating
-#else:
-#    app.config = AppConfig(app, 'scrypture.config.py')
-
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
 print 'Starting Scrypture at http://localhost:5000'
+load_config('default_config.py')
+load_scripts()
+load_api()
 app.run(threaded=True, host='0.0.0.0', debug=True)
 
