aaeff3ad29003a5dbe71daa2c338dae3e5a89fa4
Moses Schwartz <mschwar1@bechtel.com>
2015-08-03 17:49:34 -0700

    version++

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.5',
+    version='0.2.6',
 
     description='Automatically serve Python scripts through a web interface and REST API.',
     long_description=long_description,
